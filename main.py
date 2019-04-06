b47b8edc1b4fb33d96c4067147bfcd1b3ce61936
Akash Bhave <akashbbhave@gmail.com>
2019-04-06 17:43:50 -0400

    Add external tests to main.py
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from modules import external
 
 def start():
     print("TEST")
-    external.get_temp()
-    external.get_hum()
+    print("Temperature (C): ", external.get_temp())
+    print("Altitude (m): ", external.get_altitude())
+    print("Pressure (kPa):", external.get_pressure())
 
 
 if __name__ == "__main__":
