e0c230da524f6ebc3e5d9a94ad1ca20c26037416
Akash Bhave <akashbbhave@gmail.com>
2019-04-06 14:54:48 -0400

    Create external.py, update main.py
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from modules import external
+
 def start():
     print("TEST")
+    external.get_temp()
+    external.get_hum()
 
 
 if __name__ == "__main__":
---
+++ modules/external.py
@@ -0,0 +1,6 @@
+def get_temp():
+    pass
+
+
+def get_hum():
+    pass
