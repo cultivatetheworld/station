df0f0286c514b5278c7bf4f2e32656fde78b0939
Akash Bhave <akashbbhave@gmail.com>
2019-04-07 06:44:55 -0400

    Allowing syncing with Firebase
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from modules import external
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import time
+from random import randint
+
+
+cred = credentials.Certificate('secret.json')
+default_app = firebase_admin.initialize_app(cred,  {'databaseURL': 'https://cultivatetheworld-d01f7.firebaseio.com/m'})
+root = db.reference("")
 
 def start():
-    print("Temperature (C): ", external.get_temp())
-    print("Altitude (m): ", external.get_altitude())
-    print("Pressure (kPa):", external.get_pressure())
-    print("Relative humidity (%)", external.get_humidity())
+    while True:
+        root.update({"temp": external.get_temp()})
+        root.update({"altitude": external.get_altitude()})
+        root.update({"pressure": external.get_pressure()})
+        root.update({"humidity": external.get_humidity()})
+
+        # root.update({"temp": randint(0, 10)})
+        # root.update({"altitude": randint(0, 10)})
+        # root.update({"pressure": randint(0, 10)})
+        # root.update({"humidity": randint(0, 10)})
+
+        time.sleep(5)
 
 
 if __name__ == "__main__":
